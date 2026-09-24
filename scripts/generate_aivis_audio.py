fb96abdc1d149e84f61335d106c570abb81f9e27
Sergey. A. Chebotarev <chebotarev_sa@mobui.ru>
2026-09-24 08:55:25 +0300

    Add home tour and kitchen Japanese vocabulary cards

--- scripts/generate_aivis_audio.py
+++ scripts/generate_aivis_audio.py
@@ -28,10 +28,10 @@ DEFAULT_SPEED_SCALE = 0.9
 
 # Разрешённые голоса (точное имя speaker в ответе /speakers локального Engine).
 ALLOWED_TTS_SPEAKERS: tuple[str, ...] = (
+    "かりん(現実20代女子AIボイチェン@リアボVC公式モデル)",
     "阿井田 茂",
     "にせ",
-    "花音",
-    "みちのくあいり",
+    "わかな(現実20代女子AIボイチェン@リアボVC公式モデル)",
 )
 HTML_TAG_RE = re.compile(r"<[^>]+>")
 EXAMPLE_SPLIT_RE = re.compile(r"\s*<br\s*/?>\s*|\r?\n")
